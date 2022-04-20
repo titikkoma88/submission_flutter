f1b8766abace0b7abd948a9c263d96134c6229bc
Panji Ramadhan <panji.ramadhan@hadeai.com>
2022-04-20 10:29:48 +0700

    Web Page

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,20 +1,259 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'model/course.dart';
 
 var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
 
-class DetailScreen extends StatefulWidget {
+class DetailScreen extends StatelessWidget {
   final Courses course;
 
-  DetailScreen({required this.course});
+  const DetailScreen({required this.course});
 
   @override
-  State<DetailScreen> createState() => _DetailScreenState();
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 800) {
+          return DetailWebPage(course: course);
+        } else {
+          return DetailMobilePage(course: course);
+        }
+      },
+    );
+  }
 }
 
-class _DetailScreenState extends State<DetailScreen> {
+class DetailWebPage extends StatefulWidget {
+  final Courses course;
+
+  const DetailWebPage({required this.course});
+
+  @override
+  State<DetailWebPage> createState() => _DetailWebPageState();
+}
+
+class _DetailWebPageState extends State<DetailWebPage> {
+  @override
+  Widget build(BuildContext context) {
+    Future<void> showSuccessDialog() async {
+      return showDialog(
+        context: context,
+        builder: (BuildContext context) => Container(
+          child: AlertDialog(
+            backgroundColor: Colors.grey,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        Icons.close,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'Success :)',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'anda sudah terdaftar pada kelas ini',
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Container(
+                    width: 154,
+                    height: 44,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.black54,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: Text(
+                        'mulai kelas',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 16,
+            horizontal: 64,
+          ),
+          child: Center(
+            child: Container(
+              width: 1200,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(
+                        fontFamily: 'Staatliches',
+                        fontSize: 32,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/main');
+                    },
+                    child: const Text('Dicoding'),
+                  ),
+                  SizedBox(height: 32),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              child: Image.asset(widget.course.imageAsset),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            SizedBox(height: 16),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 32),
+                      Expanded(
+                        child: Card(
+                          child: Container(
+                            padding: const EdgeInsets.all(16),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[
+                                Container(
+                                  child: Text(
+                                    widget.course.name,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 30.0,
+                                      fontFamily: 'Staatliches',
+                                    ),
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: <Widget>[
+                                        Icon(Icons.calendar_today),
+                                        SizedBox(width: 8.0),
+                                        Text(
+                                          widget.course.days,
+                                          style: informationTextStyle,
+                                        ),
+                                      ],
+                                    ),
+                                    FavoriteButton(),
+                                  ],
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Icon(Icons.access_time),
+                                    SizedBox(width: 8.0),
+                                    Text(
+                                      widget.course.time,
+                                      style: informationTextStyle,
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 8.0),
+                                Row(
+                                  children: <Widget>[
+                                    Icon(Icons.monetization_on),
+                                    SizedBox(width: 8.0),
+                                    Text(
+                                      widget.course.price,
+                                      style: informationTextStyle,
+                                    ),
+                                  ],
+                                ),
+                                Container(
+                                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                                  child: Text(
+                                    widget.course.description,
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontFamily: 'Oxygen',
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  height: 50,
+                                  width: double.infinity,
+                                  margin: EdgeInsets.all(15),
+                                  child: TextButton(
+                                      onPressed: () {
+                                        showSuccessDialog();
+                                      },
+                                      style: TextButton.styleFrom(
+                                          backgroundColor: Colors.black87,
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(12))),
+                                      child: Text(
+                                        'Belajar Sekarang',
+                                      )),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DetailMobilePage extends StatelessWidget {
+  final Courses course;
+
+  const DetailMobilePage({required this.course});
+
   @override
   Widget build(BuildContext context) {
     Future<void> showSuccessDialog() async {
@@ -97,7 +336,7 @@ class _DetailScreenState extends State<DetailScreen> {
           children: <Widget>[
             Stack(
               children: <Widget>[
-                Image.asset(widget.course.imageAsset),
+                Image.asset(course.imageAsset),
                 SafeArea(
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
@@ -126,7 +365,7 @@ class _DetailScreenState extends State<DetailScreen> {
             Container(
               margin: EdgeInsets.only(top: 16.0),
               child: Text(
-                widget.course.name,
+                course.name,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 30.0,
@@ -145,7 +384,7 @@ class _DetailScreenState extends State<DetailScreen> {
                       Icon(Icons.calendar_today),
                       SizedBox(height: 8.0),
                       Text(
-                        widget.course.days,
+                        course.days,
                         style: informationTextStyle,
                       ),
                     ],
@@ -155,7 +394,7 @@ class _DetailScreenState extends State<DetailScreen> {
                       Icon(Icons.access_time),
                       SizedBox(height: 8.0),
                       Text(
-                        widget.course.time,
+                        course.time,
                         style: informationTextStyle,
                       ),
                     ],
@@ -165,7 +404,7 @@ class _DetailScreenState extends State<DetailScreen> {
                       Icon(Icons.monetization_on),
                       SizedBox(height: 8.0),
                       Text(
-                        widget.course.price,
+                        course.price,
                         style: informationTextStyle,
                       ),
                     ],
@@ -176,7 +415,7 @@ class _DetailScreenState extends State<DetailScreen> {
             Container(
               padding: EdgeInsets.all(16.0),
               child: Text(
-                widget.course.description,
+                course.description,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 16.0,
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -8,51 +8,130 @@ class MainScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: Text('Dicoding'),
+        title: Text(
+          'DICODING',
+        ),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Courses course = coursesList[index];
-          return InkWell(
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return DetailScreen(course: course);
-              }));
-            },
-            child: Card(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Image.asset(course.imageAsset),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(
-                            course.name,
-                            style: TextStyle(fontSize: 16.0),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(course.title),
-                        ],
-                      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 600) {
+            return CoursesList();
+          } else if (constraints.maxWidth <= 1200) {
+            return CoursesGrid(gridCount: 4);
+          } else {
+            return CoursesGrid(gridCount: 6);
+          }
+        },
+      ),
+    );
+  }
+}
+
+class CoursesList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final Courses course = coursesList[index];
+        return InkWell(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return DetailScreen(course: course);
+            }));
+          },
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Image.asset(course.imageAsset),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          course.name,
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(course.title),
+                      ],
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          );
-        },
-        itemCount: coursesList.length,
+          ),
+        );
+      },
+      itemCount: coursesList.length,
+    );
+  }
+}
+
+class CoursesGrid extends StatelessWidget {
+  final int gridCount;
+
+  CoursesGrid({required this.gridCount});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      isAlwaysShown: true,
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: GridView.count(
+          crossAxisCount: gridCount,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          children: coursesList.map((course) {
+            return InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return DetailScreen(course: course);
+                }));
+              },
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        course.imageAsset,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        course.name,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                      child: Text(
+                        course.title,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
